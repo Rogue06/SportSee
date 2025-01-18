7dadbc6b3070a534dda2e9e7c3ce2ae0804c20a3
Rogue06 <111644419+Rogue06@users.noreply.github.com>
2025-01-18 17:43:43 +0100

    feat: setup project structure and mock data configuration

---
+++ SportSee-frontend/src/mocks/data.js
@@ -0,0 +1,19 @@
+// Copie des données du backend pour le développement initial
+export const USER_MAIN_DATA = [
+  {
+    id: 12,
+    userInfos: {
+      firstName: "Karl",
+      lastName: "Dovineau",
+      age: 31,
+    },
+    todayScore: 0.12,
+    keyData: {
+      calorieCount: 1930,
+      proteinCount: 155,
+      carbohydrateCount: 290,
+      lipidCount: 50,
+    },
+  },
+  // ... autres données
+];
